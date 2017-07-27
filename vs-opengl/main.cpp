f4d1a6e9d9d13926354442f3f28ed47626abfa19
Xiong <yuan.xiong@intel.com>
2017-07-27 13:56:01 +0800

    update argc and argv for glutinit.

--- vs-opengl/main.cpp
+++ vs-opengl/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 #include <openglxy\glut.h>
 
@@ -12,8 +13,12 @@
 // 一年12个月，共是360天  
 static int day = 200; // day的变化：从0到359  
 
+const GLfloat factor = 0.1f;
+
 double CalFrequency()
 {
+	std::cout << "CalFrequency!" << std::endl;
+
 	static int count;
 	static double save;
 	static clock_t last, current;
@@ -32,6 +37,8 @@ double CalFrequency()
 
 void myDisplay(void)
 {
+	std::cout << "myDisplay!" << std::endl;
+
 	double FPS = CalFrequency();
 	printf("FPS = %f\n", FPS);
 
@@ -46,7 +53,7 @@ void myDisplay(void)
 	gluLookAt(0, -200000000, 200000000, 0, 0, 0, 0, 0, 1);
 
 	// 绘制红色的“太阳”  
-	glColor3f(1.0f, 0.0f, 0.0f);
+	glColor3f(0.5f, 0.0f, 0.0f);
 	glutSolidSphere(69600000, 20, 20);
 	// 绘制蓝色的“地球”  
 	glColor3f(0.0f, 0.0f, 1.0f);
@@ -65,6 +72,8 @@ void myDisplay(void)
 
 void myIdle(void)
 {
+	std::cout << "myIdle!" << std::endl;
+
 	++day;
 	if (day >= 360)
 		day = 0;
@@ -72,6 +81,7 @@ void myIdle(void)
 }
 
 void openglTest1() {
+	std::cout << "openglTest1!" << std::endl;
 
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowPosition(100, 100);
@@ -99,14 +109,49 @@ void openglTest2() {
 	glutMainLoop();
 }
 
+void myDisplay3(void)
+{
+	GLfloat x;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINES);
+	glVertex2f(-1.0f, 0.0f);
+	glVertex2f(1.0f, 0.0f);         // 以上两个点可以画x轴
+	glVertex2f(0.0f, -1.0f);
+	glVertex2f(0.0f, 1.0f);         // 以上两个点可以画y轴
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	for (x = -1.0f / factor; x<1.0f / factor; x += 0.01f)
+	{
+		glVertex2f(x*factor, sin(x)*factor);
+	}
+	glEnd();
+
+	glFlush();
+}
+
+void openglTest3() {
+	std::cout << "openglTest3!" << std::endl;
+
+	int pargc = 1;
+	char *pargv = "openglTest3";
+	char** vargc = &pargv;
+	glutInit(&pargc, vargc);
+
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(400, 400);
+	glutCreateWindow("第3个OpenGL程序");
+	glutDisplayFunc(&myDisplay3);
+	glutMainLoop();
+}
+
 int main(int argc, char *argv[])
 {
 	std::cout << "Hello VS, this is the main function!" << std::endl;
 	
-	glutInit(&argc, argv);
-	
 	//openglTest1();
-	openglTest2();
+	//openglTest2();
+	openglTest3();
 
 
 	return 0;
